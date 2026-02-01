c072fdc9270f5752084dde9206b676998dd16f0d
MalikMohamad <jayshiv0380@gmail.com>
2026-02-01 19:12:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Your Google Apps Script URL
-const FORM_URL = "https://script.google.com/macros/s/AKfycbzoPWVZeTx8DZBMSftGqv--kAQvPt3c3avDOOsUwcL-voW8Uy2SFHVpY1HoWbgma1qy3Q/exec";
+const FORM_URL = "https://script.google.com/macros/library/d/19LANoe5kuHJG2XXSoWsTn1rB-BL3FWEirgNGyY5BK5AwjQyxU2BDr6N3/1";
 
 document.getElementById("fullForm").addEventListener("submit", function (e) {
   e.preventDefault();
